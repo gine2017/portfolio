87e80106955f0b017b28e42b065ad558438ef40c
Regine Thimothee <ginou10@msn.com>
2025-05-12 08:45:31 -0400

    sneak peak for case studies

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -76,6 +76,19 @@ export default function Navbar() {
               projects
             </Link>
           </li>
+          <li className="transition-transform hover:-translate-y-1">
+            <Link
+              rel="noopener noreferrer"
+              href={"/illustrations"}
+              className={`${
+                currentPath === "/illustrations"
+                  ? "active: underline underline-offset-[12px] p-2"
+                  : "text-[#597891] hover:text-purple"
+              } `}
+            >
+              illustrations
+            </Link>
+          </li>
           <li className="transition-transform text-[#597891] hover:-translate-y-1 hover:text-purple">
             <a
               href={"/files/Regine-Thimothee-Resume.pdf"}
